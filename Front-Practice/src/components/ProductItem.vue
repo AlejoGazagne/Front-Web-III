5d5789dea0a98acf94455f0925c0f66f1b312886
AlejoGazagne <gazagnealejo@gmail.com>
2024-08-24 15:13:47 -0300

    borrado de productos

--- Front-Practice/src/components/ProductItem.vue
+++ Front-Practice/src/components/ProductItem.vue
@@ -1,32 +1,88 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    product: {
+        type: Object,
+        required: true
+    }
+})
+const emit = defineEmits<{
+    (event: 'delete', id: number): void;
+}>();
+
+function deleteProduct() {
+    emit('delete', props.product.id);
+}
+</script>
+
 <template>
-    <div class="product-card">
-        <img src="../assets/suit-2.png" alt="Producto">
+    <div class="product-card" v-if="product.rating.rate > 4">
+        <img :src="product.image" :alt="product.title">
         <div class="product-info">
-            <h2>Titulo</h2>
-            <p>descripccion</p>
-            <p>Precio</p>
+            <h2>{{ product.title }}</h2>
+            <p>{{ product.category }}</p>
+            <p>Price: ${{ product.price }}</p>
+        </div>
+        <div class="buttons">
+            <button>Editar</button>
+            <button @click="deleteProduct">Borrar</button>
         </div>
     </div>
 </template>
+
 <style scoped>
-.product-card{
+.product-card {
     margin: 1rem;
-    border: 1px solid #cccccc93;
+    border: 1px solid #bebebe93;
     border-radius: 15px;
-    background-color: #f0ebfc8e;
+    background-color: #cacaca8e;
     transition-duration: .3s;
     width: min-content;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 }
-.product-card:hover{
-    transform: matrix(1.05, 0, 0, 1.05, 0, 0);
-    box-shadow: 0 0 15px #5252523d;
+
+.product-card:hover {
+    /* transform: matrix(1.05, 0, 0, 1.05, 0, 0); */
+    box-shadow: 0 0 15px #52525298;
     cursor: pointer;
 }
-img{
+
+img {
     border-radius: 15px 15px 0 0;
+    width: 250px;
+    height: 250px;
+    object-fit: cover;
 }
-.product-info{
+
+.product-info {
     padding: .5rem 1rem;
+}
+
+.buttons {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: .8rem;
+}
+
+button {
+    padding: .5rem 1rem;
+    border: none;
+    border-radius: 5px;
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+    background-color: #515cb1;
+    transition-duration: .3s;
+}
+
+button:hover {
+    filter: brightness(1.4);
+}
+
+.buttons button:last-child {
+    background-color: rgb(184, 0, 0);
 }
 </style>
--- Front-Practice/src/components/ProductList.vue
+++ Front-Practice/src/components/ProductList.vue
@@ -1,32 +1,57 @@
 <script setup lang="ts">
-// importar ProductItem
 import ProductItem from './ProductItem.vue'
+import { onMounted, ref } from 'vue';
+
+const products = ref([])
+const isFinished = ref(false)
+
+const fetchProducts = async () => {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products')
+        const data = await response.json()
+        products.value = data
+    } catch (error) {
+        console.error('Error al obtener productos:', error)
+    } finally {
+        isFinished.value = true
+    }
+}
+
+onMounted(() => {
+    fetchProducts()
+})
+
+function deleteProduct(id: number) {
+    products.value = products.value.filter(product => product.id !== id)
+}
+
 </script>
+
 <template>
     <main>
         <p>Lista de productos</p>
-        <section class="catalogo">
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
+        <h3 v-if="!isFinished">Cargando caralogo... </h3>
+        <section v-else class="catalogo">
+            <ProductItem v-for="product in products" :key="product.id" :product="product" @delete="deleteProduct" />
         </section>
     </main>
 </template>
+
 <style scoped>
 main {
     margin: 5rem 5%;
 }
-main p{
+
+main p {
     font-size: 1.5rem;
     font-weight: bold;
 }
-.catalogo{
+
+.catalogo {
     display: flex;
     justify-content: center;
     align-items: center;
     flex-wrap: wrap;
+    align-items: stretch;
 }
 </style>
