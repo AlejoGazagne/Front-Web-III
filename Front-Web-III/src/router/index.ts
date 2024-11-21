ce393652c3e83ceb18e2efb0ab726a9985b8d880
AlejoGazagne <gazagnealejo@gmail.com>
2024-11-21 11:59:35 -0300

    unificación de codigo

--- Front-Web-III/src/router/index.ts
+++ Front-Web-III/src/router/index.ts
@@ -1,19 +1,63 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import { useAuthStore } from '../stores/userAuthStore';
+
+const routes = [
+  // Rutas de autenticación
+  {
+    path: '/login',
+    component: () => import('../views/auth/LoginView.vue'),
+  },
+  // Rutas del administrador
+  {
+    path: '/admin',
+    component: () => import('@/views/admin/AddProductView.vue'), // Cambiar por la vista del admin
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/admin/AlertsView.vue'),
+      },
+      {
+        path: 'manage-users',
+        component: () => import('@/views/admin/ManageUsersView.vue'),
+      },
+    ],
+    meta: { requiresAdmin: true, role: 'admin' },
+  },
+  // Rutas del Operator
+
+  // Rutas comparidas
+  {
+    path: '/',
+    component: () => import('../views/shared/HomeView.vue'),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/dashboard',
+    component: () => import('../views/shared/DashboardView.vue'),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/profile',
+    component: () => import('../views/shared/ProfileView.vue'),
+    meta: { requiresAuth: true },
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/auth/LoginView.vue'),
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/layouts/HomeView.vue'),
-    }
-  ],
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next('/login');// ver si se puede cambiar y hacer un log automatico asi no pierde las cosas
+  } else if (to.meta.role && to.meta.role !== authStore.userRole) {
+    next('/');
+  } else {
+    next();
+  }
+});
+
+export default router;
