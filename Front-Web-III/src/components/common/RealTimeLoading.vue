d0eac72df8f1d77d9a7b6297a514b9736bdd1255
AgustinRagessi <gragess561@alumnos.iua.edu.ar>
2025-02-17 19:42:57 -0300

    redesigned loading truck screen

--- Front-Web-III/src/components/common/RealTimeLoading.vue
+++ Front-Web-III/src/components/common/RealTimeLoading.vue
@@ -1,27 +1,55 @@
 <template>
   <v-container>
     <div class="p-4">
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" style="width:40%">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" style="width:85%;">
           <div v-for="[externalId, updates] in sortedOrders" :key="externalId" class="bg-white shadow-lg p-4 rounded-lg mb-4">
-          <h3 class="text-h6 mb-2 pt-2 pl-2 pr-2 border-b-lg border-opacity-75 font-weight-black font-italic text-decoration-underline">Orden #{{ externalId }}</h3>
-            <ul class="text-sm">
+          <h3 class="text-h6 mb-2 pt-2 pl-2 pr-2 border-b-lg border-info border-opacity-75 font-weight-black font-italic text-decoration-underline" style="text-align: center;">Orden #{{ externalId }}</h3>
+            <ul class="text-sm">  
               <li v-for="(update, index) in updates" :key="index" class="mb-2" style="list-style-type: none;">
-                <strong class="ml-6 text-body-1">Masa:</strong> <span class="text-body-1">{{ update.accumulatedMass }} kg</span> <br />
-                <strong class="ml-6 text-body-1">Caudal:</strong> <span class="text-body-1">{{ update.caudal }} L/s</span> <br />
-                <strong class="ml-6 text-body-1">Temperatura:</strong> <span class="text-body-1">{{ update.temperature }} °C</span> <br />
-                <strong class="ml-6 text-body-1">Fecha actual:</strong> <span class="text-body-1">{{ formatDate(update.timestampLoad) }}</span> <br />
-
+                <strong class="ml-6 mdi mdi-circle" style="font-size: medium;"> Masa:</strong> <span>{{ update.accumulatedMass }} kg</span> <br />
+                <strong class="ml-6 mdi mdi-pipe" style="font-size: medium;"> Caudal:</strong> <span>{{ update.caudal }} L/s</span> <br />
+                <strong class="ml-6 mdi mdi-thermometer" style="font-size: medium;"> Temperatura:</strong> <span>{{ update.temperature }} °C</span> <br />
+                <strong class="ml-6 mdi mdi-calendar" style="font-size: medium;"> Fecha actual:</strong> <span>{{ formatDate(update.timestampLoad) }}</span> <br />
+                
                 <v-btn small class="ml-6 mt-2" color="primary" @click="viewMore(update.externalId, index)">
-                  {{ expandedIndex === index ? "Quitar detalles" : "Ver Más" }}
+                  {{ expandByIndex[externalId] === index ? "Quitar detalles" : "Ver Más" }}
                 </v-btn>
 
-                <div v-if="expandedIndex === index" class="mt-2 ml-6">
-                  <strong>Patente de camión:</strong> <span>{{ expandedOrder?.truck.plate || "N/A" }}</span> <br />
-                  <strong>Producto:</strong> <span>{{ expandedOrder?.product.name || "N/A" }}</span> <br />
-                  <strong>Conductor:</strong> <span>{{ expandedOrder?.driver.name || "N/A" }} {{ expandedOrder?.driver.lastname || "N/A" }}</span> <br />
-                  <strong>Cliente:</strong> <span>{{ expandedOrder?.client.companyName || "N/A" }}</span> <br />
-                  <strong>Tiempo de finalización aproximado:</strong> <span>{{ formatDate(calculateETA(expandedOrder, update)) }}</span> <br />
-                </div>
+                <div v-if="expandByIndex[externalId] === index" class="mt-2 ml-6">
+                    <div style="display: flex; flex-direction: column;">
+                    <div style="display: flex; justify-content: space-around; margin-bottom: 10px;">
+                      <div>
+                      <strong class="mdi mdi-domain"> Cliente:</strong>
+                      <li class="ml-6" style="list-style-type: disc;">
+                        <strong> Nombre:</strong> <span>{{ expandedOrder[externalId]?.client.companyName || "N/A" }}</span>
+                      </li>
+                      </div>
+                      <div>
+                      <strong class="mdi mdi-truck"> Camión:</strong>
+                        <ul class="ml-6">
+                          <li style="list-style-type: disc;">
+                            <strong>Patente:</strong> <span>{{ expandedOrder[externalId]?.truck.plate || "N/A" }}</span>
+                          </li>
+                          <li style="list-style-type: disc;">
+                            <strong>Conductor:</strong> <span>{{ expandedOrder[externalId]?.driver.name || "N/A" }} {{ expandedOrder[externalId].driver.lastname }}</span>
+                          </li>
+                        </ul>
+                      </div>
+                      <div>
+                        <strong class="mdi mdi-package"> Producto:</strong>
+                        <ul class="ml-6">
+                          <li style="list-style-type: disc;">
+                            <strong>Nombre:</strong> <span>{{ expandedOrder[externalId]?.product.name || "N/A" }}</span>
+                          </li>
+                          <li style="list-style-type: disc;">
+                            <strong>Temperatura Límite:</strong> <span>{{ expandedOrder[externalId]?.product.limit_temperature || "N/A" }}</span>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    </div>
+                <br />
+                  <div><strong class="mdi-timer mdi">Tiempo de finalización aproximado:</strong> <span>{{ formatDate(calculateETA(expandedOrder[externalId] , update)) }}</span> </div>                </div>
                 <v-card-text>
                   <ProgressBar :currentMass="update.accumulatedMass" :externalId="update.externalId" />
                 </v-card-text>
@@ -67,8 +95,8 @@
     }
   };
   const orders = ref({});
-  const expandedIndex = ref(null);
-  const expandedOrder = ref(null);
+  const expandByIndex = ref({});
+  const expandedOrder = ref({});
   onMounted(() => {
     subscribe(topicOrder, handleMessage);
 
@@ -90,22 +118,22 @@
   }
 
   const viewMore = async (externalId, index) => {
-    if (expandedIndex.value === index) {
-      expandedIndex.value = null; // Collapse the section
-      expandedOrder.value = null;
+    if (expandByIndex.value[externalId] === index) {
+      delete expandByIndex.value[externalId]; 
+      delete expandedOrder.value[externalId];
       return;
     }
 
-    expandedIndex.value = index; // Set expanded index
-    expandedOrder.value = null; // Reset data while fetching
+    expandByIndex.value[externalId] = index; 
+    expandedOrder.value[externalId] = null; 
 
     try {
-      const response = await ordersStore.fetchById(externalId); // Wait for promise resolution
-      expandedOrder.value = response; // Store the resolved object
+      const response = await ordersStore.fetchById(externalId); 
+      expandedOrder.value[externalId] = response; 
       console.log("Expanded data:", expandedOrder.value);
     } catch (error) {
       console.error("Error fetching order details:", error);
-      expandedOrder.value = { error: "Failed to load details" };
+      expandedOrder.value[externalId] = { error: "Failed to load details" };
     }
   };
 
@@ -118,11 +146,6 @@
     const remainingTime = remainingMass / loadData.caudal;
     const currentTimestamp = new Date(loadData.timestampLoad);
     const eta = new Date(currentTimestamp.getTime() + remainingTime * 1000);
-    console.log("y ahora que");
-    console.log(remainingMass);
-    console.log(remainingTime);
-    console.log(currentTimestamp);
-    console.log(eta);
     return eta;
     
   };
