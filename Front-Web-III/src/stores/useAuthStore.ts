a19df8f250e9e4610b4b54c1b346317cd6c5ec1f
Alejo Gazagne <64871596+AlejoGazagne@users.noreply.github.com>
2025-11-13 15:08:12 -0300

    prueba
    agrego esta variable que no sirve para poder detectarla en n8n

--- Front-Web-III/src/stores/useAuthStore.ts
+++ Front-Web-III/src/stores/useAuthStore.ts
@@ -7,6 +7,7 @@ export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: localStorage.getItem('token'),
     userData: JSON.parse(localStorage.getItem('userData') || '{}') as UserData,
+    aws: alejo123123123alejo
   }),
   actions: {
     async login(username: string, password: string) {
